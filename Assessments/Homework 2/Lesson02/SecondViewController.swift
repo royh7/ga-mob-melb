2c77b60cb820a6e25382e0c347253e28a533a180
Antonio de Perio <antoniodeperio@gmail.com>
2015-12-04 12:27:27 +1100

    adding assignment 3 starting app

--- Assessments/Homework 2/Lesson02/SecondViewController.swift
+++ Assessments/Homework 2/Lesson02/SecondViewController.swift
@@ -10,5 +10,29 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
     //TODO five: Display the cumulative sum of all numbers added every time the ‘add’ button is pressed. Hook up the label, text box and button to make this work.
+    
+    var myArray = [Int]()
+    
+    @IBAction func add(sender: AnyObject)
+    {
+        
+        let myInt = Int(textField!.text!)
+        
+        var sum = 0
+        var counter = 0
+        myArray.append(myInt!)
+        
+        while counter < myArray.count {
+            let newValue = myArray[counter]
+            sum += newValue
+            counter++
+        }
+        self.resultLabel!.text = String(sum)
+        //myArray.append(newValue)
+    }
 }
