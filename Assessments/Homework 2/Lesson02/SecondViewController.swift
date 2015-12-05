7b22dda922dd3d3a20e821df9cfe7e3edfbfc57a
Roy Ho <royho_ern@hotmail.com>
2015-12-05 23:02:03 +1100

    no message

--- Assessments/Homework 2/Lesson02/SecondViewController.swift
+++ Assessments/Homework 2/Lesson02/SecondViewController.swift
@@ -11,4 +11,21 @@ import UIKit
 class SecondViewController: UIViewController {
 
     //TODO five: Display the cumulative sum of all numbers added every time the ‘add’ button is pressed. Hook up the label, text box and button to make this work.
+    // add each added number to a list, +1 to the right in a variable then add that variable int.
+    
+    @IBOutlet weak var NumberField: UITextField?
+    
+    @IBOutlet weak var CumulativeNumber: UILabel?
+    
+    @IBOutlet weak var AddButton: UIButton?
+    
+    var sum = 0
+    var numbers : [Int]? = []
+    
+    @IBAction func AddNumberToList(sender: AnyObject) {
+        let newnumber = Int(NumberField!.text!)
+        numbers!.append(newnumber!)
+        sum = sum + numbers!.last!
+        self.CumulativeNumber!.text = String(sum)
+    }
 }
--- Assessments/Homework 2/Lesson02/ThirdViewController.swift
+++ Assessments/Homework 2/Lesson02/ThirdViewController.swift
@@ -9,8 +9,26 @@
 import UIKit
 
 class ThirdViewController: UIViewController {
-/*
-    TODO six: Hook up the number input text field, button and text label to this class. When the button is pressed, a message should be printed to the label indicating whether the number is even.
+//  TODO six: Hook up the number input text field, button and text label to this class. When the button is pressed, a message should be printed to the label indicating whether the number is even.
 
-*/
+    @IBOutlet weak var InputNumber: UITextField?
+    
+    @IBOutlet weak var OddOrEvenNumber: UILabel?
+    
+    @IBOutlet weak var CalculateButton: UIButton?
+    
+    @IBAction func OddOrEven (sender: AnyObject?) {
+        let test = InputNumber!.text
+        if (test != nil) {
+            if (Int(InputNumber!.text!)! % 2 == 0)
+            {
+                self.OddOrEvenNumber!.text = String("The number is even")
+            }
+            else
+            {
+                self.OddOrEvenNumber!.text = String("The number is not even")
+            }
+        }
+    
+}
 }
