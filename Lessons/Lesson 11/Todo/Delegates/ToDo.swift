f3d50557ee3e3b548c41cfe9489c7cb852b6fd62
Antonio de Perio <antoniodeperio@gmail.com>
2015-12-18 11:47:24 +1100

    lesson 10

---
+++ Lessons/Lesson 11/Todo/Delegates/ToDo.swift
@@ -0,0 +1,23 @@
+//
+//  ToDo.swift
+//  Delegates
+//
+//  Created by Antonio de Perio on 10/12/2015.
+//  Copyright © 2015 Workshop. All rights reserved.
+//
+
+import Foundation
+
+
+class Todo {
+
+    var taskName : String
+    var taskDescription : String
+    
+    init(name: String, desc: String){
+    
+        self.taskName = name
+        self.taskDescription = desc
+    }
+
+}
